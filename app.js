bb27de4df852fb54d3cf899522e90a5c625f5be1
gabrielroot <gabrielfer.s88@gmail.com>
2020-06-27 18:26:46 -0300

    forçando ssl

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const app = express()
 const nunjucks = require('nunjucks')
 const routes = require('./src/routes/routes')
 
+const forceSsl = function (req, res, next) {   //Redirecionamento automático de HTTP para HTTPS
+    if (req.headers['x-forwarded-proto'] !== 'https'  && req.hostname !== 'localhost') {
+        console.log('REDIRECT HTTP > HTTPS')
+        return res.redirect(['https://', req.get('Host'), req.url].join(''))
+    }
+    return next()
+}
+app.use(forceSsl)
+
 nunjucks.configure('./src/views',{
     express: app,
     noCache: !false
