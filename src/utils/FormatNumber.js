24d5c5dec193c2c552fa9e31c5cef61537434e96
gabrielroot <gabrielfer.s88@gmail.com>
2020-07-17 15:38:33 -0300

    corrigindo regex (x|0<x<50)

--- src/utils/FormatNumber.js
+++ src/utils/FormatNumber.js
@@ -2,8 +2,8 @@ const FormatNumber = {
     toREAL(value){//Para valor COM o cifrão
         return `R$ ${value.toFixed(2).replace('.',',')}`
     },
-    toQtde(value){//Para validar se o número tem '.', ',' , 'e', é >= 50, tem 3 dígitos ou é negativo
-        return  `${value.toString().replace(/([5-9][0-9]*)|([-]+[0-9]*)|([\d]{3})|\D/g,'')}`
+    toQtde(value){//Para validar se o número tem '.', ',' , 'e', é < 50, tem 3 dígitos ou é negativo
+        return  `${value.toString().replace(/([5-9]\d|[1-9]\d{2,})|(50)|\D/g,'')}`
     },
     isOverHundred(value){//Verifica se é menor que 100
         return `${value.toString().replace(/([1-9][1-9]\d)|([1-9]\d[1-9])|(\d{4})/g,'')}`
