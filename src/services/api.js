1eff731d63552f8e0b75d8fab033b6290689f962
gabrielroot <gabrielfer.s88@gmail.com>
2020-10-28 19:44:12 -0300

    Env DEV

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
 require('dotenv/config')
-const baseURL = process.env.REACT_APP_BASE_URL
 
+let baseURL
+
+if(window.location.hostname === 'localhost')
+    baseURL = process.env.REACT_APP_BASE_URL_DEV
+else
+    baseURL = process.env.REACT_APP_BASE_URL
 
 const Api = axios.create({ baseURL })
 
